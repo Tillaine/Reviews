6b916950f6bc2553b0c599efc7f192a180041920
Tillaine <55994239+Tillaine@users.noreply.github.com>
2020-03-17 13:31:32 -0600

    fixed bug

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -14,39 +14,12 @@ class App extends React.Component {
             AvgRating: 0
         }
         this.handleSearch = this.handleSearch.bind(this);
-        // this.getRatingData = this.getRatingData.bind(this);
 
     }
     // id, user_name, property_id, message, date_created, user_img_url, accuracy_rating, location_rating, cleanliness_rating, value_rating, communication_rating, Check_in_rating
     //aggregates rating data to display in ratingSummary chart
-    // getRatingData() {
-    //     const ratingSum = {};
-    //     this.state.reviews.forEach(review => {
-    //         for(let category in review) {
-    //             if(category.includes('rating')){
-    //                 if(ratingSum[category] === undefined) {
-    //                     ratingSum[category] = {all: [review[category]]};
-    //                 } else {
-    //                     ratingSum[category].all.push(review[category]) 
-    //                 }
-    //             }
-    //     }
-    //     })
-    //     const rateSumArray = Object.keys(ratingSum).map(category => {
-    //         const avg = (ratingSum[category].all.reduce((total, num) => 
-    //         total + num)/ratingSum[category].all.length).toFixed(1);
-    //         ratingSum[category].sum = avg;
-    //         return avg
-    //     })
-    //     this.setState({
-    //         AvgRating: (rateSumArray.reduce((total, rate) => { 
-    //             return Number(total) + Number(rate)})/rateSumArray.length).toFixed(1), 
-    //         ratings: ratingSum,
-    //         gotRatings: true
-
-    //     })
-
-    // }
+    
+   
 
     handleSearch(searchTerm) {
         const options = {
@@ -65,8 +38,8 @@ class App extends React.Component {
         fetch('http://localhost:3000/api/reviews')
         .then(reviews => reviews.json())
         .then(reviews => {
-            console.log("reviews:", reviews)
-            this.setState({reviews, ratings, AvgRating: reviews.ratings.sum})
+            this.setState({reviews: reviews.reviews, ratings: reviews.ratings, AvgRating: reviews.ratings.sum})
+            console.log("reviews:", this.state.reviews)
             
         })
         
@@ -87,7 +60,7 @@ class App extends React.Component {
                         <RatingSummary2 ratings={ this.state.ratings } avg={ this.state.AvgRating } gotRatings={ this.state.gotRatings }/>
                     </div>
 
-                </div> */}
+                </div> 
                 <ReviewList reviews={ this.state.reviews }/>
             </div>
         )
